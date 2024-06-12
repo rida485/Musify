da18aaa59244f2864a89783baf269ffb02daaeb2
rida485 <redha.algeriano5@gmail.com>
2024-06-12 12:31:07 +0200

    name

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ import './globals.css'
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Spotify Clone',
-  description: 'Spotify Clone',
+  title: 'Musify',
+  description: 'Musify',
 }
 
 export const revalidate = 0; //we don't want this layout to cache 
